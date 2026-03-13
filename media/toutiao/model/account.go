ffd62a5572302654cf51e312d3f46182572bbccf
tangping <442353346@qq.com>
2026-03-13 18:46:15 +0800

    A 获取升级版巨量引擎工作台下账户列表

--- media/toutiao/model/account.go
+++ media/toutiao/model/account.go
@@ -75,3 +75,54 @@ func (receiver *AccountResp) Convert() (resp *model.AccountResp, err error) {
 	resp.AdExtraData = receiver
 	return
 }
+
+// AccountQueryParams 账户查询参数
+type EbpAdvertiserListReq struct {
+	accessTokenReq
+	EnterpriseOrganizationID int64          `json:"enterprise_organization_id,omitempty"` // 升级版巨量引擎工作台ID
+	AccountSource            string         `json:"account_source,omitempty"`             // 账户类型，允许值：AD 巨量营销客户账号，LOCAL 本地推
+	Filtering                *AccountFilter `json:"filtering,omitempty"`                  // 过滤器
+	Page                     int            `json:"page,omitempty"`                       // 页码，默认1
+	PageSize                 int            `json:"page_size,omitempty"`                  // 页面大小，[1,100]，默认10
+}
+
+func (receiver *EbpAdvertiserListReq) Format() {
+	receiver.accessTokenReq.Format()
+	if receiver.Page <= 0 {
+		receiver.Page = 1
+	}
+	if receiver.PageSize <= 0 || receiver.PageSize > 100 {
+		receiver.PageSize = 100
+	}
+}
+
+func (receiver *EbpAdvertiserListReq) Validate() (err error) {
+	if validateErr := receiver.accessTokenReq.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	if receiver.EnterpriseOrganizationID <= 0 {
+		err = errors.New("enterprise_organization_id is empty")
+		return
+	}
+	return
+}
+
+// AccountFilter 账户过滤器
+type AccountFilter struct {
+	AccountName   string `json:"account_name,omitempty"`   // 账户名称
+	ActiveAccount *bool  `json:"active_account,omitempty"` // 是否是活跃账户
+}
+
+// EbpAdvertiserListResp 账户数据
+type EbpAdvertiserListResp struct {
+	AccountList []AccountInfo `json:"account_list,omitempty"` // 账户列表
+	PageInfo    PageInfo      `json:"page_info,omitempty"`    // 分页信息
+}
+
+// AccountInfo 账户信息
+type AccountInfo struct {
+	AccountID   int64  `json:"account_id"`   // 账户id
+	AccountType string `json:"account_type"` // 账户类型
+	AccountName string `json:"account_name"` // 账户名称
+}
