55088a3ad7ecf59bd014d16ad2b865739d2d53de
tangping <442353346@qq.com>
2026-03-19 12:10:41 +0800

    A 自定义报表获取数据&测试用例

--- media/toutiao/data_report_test.go
+++ media/toutiao/data_report_test.go
@@ -2,6 +2,7 @@ package toutiao
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/cngamesdk/media-sdk/config"
 	"github.com/cngamesdk/media-sdk/media/toutiao/model"
@@ -13,9 +14,48 @@ func TestReportCustomConfigGetSelf(t *testing.T) {
 	ctx := context.Background()
 	factory := NewToutiaoAdapter(config.DefaultConfig())
 	req := &model.ReportCustomConfigGetReq{}
-	req.AccessToken = "test"
+	req.AccessToken = "xxxx"
 	req.AdvertiserID = 123
+	req.DataTopics = []string{
+		model.DataTopicMaterial,
+	}
 	resp, err := factory.ReportCustomConfigGetSelf(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, _ := json.Marshal(resp)
+	println(fmt.Sprintf("get result: %s", string(result)))
+}
+
+// 获取自定义报表可用指标和维度
+func TestReportCustomGetGetSelf(t *testing.T) {
+	ctx := context.Background()
+	factory := NewToutiaoAdapter(config.DefaultConfig())
+	req := &model.ReportCustomGetReq{}
+	req.AccessToken = "xxxx"
+	req.AdvertiserID = 123
+	req.Dimensions = []string{
+		"stat_time_hour",
+	}
+	req.Metrics = []string{
+		"stat_cost",
+	}
+	req.StartTime = "2026-03-18 00:00:00"
+	req.EndTime = "2026-03-19 23:59:59"
+	req.Filters = []*model.FilterCondition{
+		&model.FilterCondition{
+			Field:    "stat_cost",
+			Type:     model.FieldTypeInput,
+			Operator: model.OperatorGreaterThan,
+			Values: []string{
+				"0",
+			},
+		},
+	}
+	req.OrderBy = []model.OrderBy{
+		{Field: "stat_cost", Type: model.OrderByDesc},
+	}
+	resp, err := factory.ReportCustomGetSelf(ctx, req)
 	if err != nil {
 		t.Fatal(err)
 	}
