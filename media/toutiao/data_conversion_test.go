b9378dee188fb9f44cffedf4a78f54df30fa8565
tangping <442353346@qq.com>
2026-03-20 15:47:57 +0800

    A 转化事件回调服务

--- media/toutiao/data_conversion_test.go
+++ media/toutiao/data_conversion_test.go
@@ -1,6 +1,9 @@
 package toutiao
 
 import (
+	"context"
+	"fmt"
+	"github.com/cngamesdk/media-sdk/config"
 	"github.com/cngamesdk/media-sdk/media/toutiao/model"
 	"testing"
 )
@@ -17,4 +20,18 @@ func TestClickMaros(t *testing.T) {
 
 	macros.Add("ext2", "EXT2")
 	println(macros.BuildUrl("https://www.xxx.com/?game_id=123&agent_id=123&site_id=123"))
+}
+
+// TestConversionEventReport 转化事件上报
+func TestConversionEventReport(t *testing.T) {
+	ctx := context.Background()
+	factory := NewToutiaoAdapter(config.DefaultConfig())
+	req := &model.ConversionEventReq{}
+	req.CallbackUrl = "http://ad.toutiao.com/track/activate/?callback=B.rqB7I6enkYhGUseoWmFQtyWfsFfsrOmxU"
+	req.OAID = "123"
+	resp, err := factory.DataConversionEventReportSelf(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	println(fmt.Sprintf("get result: %+v", resp))
 }
