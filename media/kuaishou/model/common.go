ae92ab3770d11358163d043bfee4191509d36de4
tangping <442353346@qq.com>
2026-03-24 11:40:38 +0800

    A 磁力引擎（快手）

---
+++ media/kuaishou/model/common.go
@@ -0,0 +1,13 @@
+package model
+
+const (
+	DevelopersUrl = "https://developers.e.kuaishou.com"
+	AdUrl         = "https://ad.e.kuaishou.com"
+)
+
+type BaseResp struct {
+	Code      int         `json:"code"`
+	Message   string      `json:"message"`
+	Data      interface{} `json:"data"`
+	RequestId string      `json:"request_id"`
+}
