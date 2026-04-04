1df97bb59cdb40a627067f5b8fec952364db96ae
tangping <442353346@qq.com>
2026-04-04 10:09:20 +0800

    A 腾讯广告修改创意组件共享&查询创意组件共享信息&获取创意组件详情逻辑

--- media/tencent/component.go
+++ media/tencent/component.go
@@ -75,3 +75,65 @@ func (a *TencentAdapter) ComponentsDeleteSelf(ctx context.Context, req *model.Co
 	resp = &result
 	return
 }
+
+// ComponentSharingUpdateSelf 修改创意组件共享
+// https://developers.e.qq.com/v3.0/docs/api/component_sharing/update
+func (a *TencentAdapter) ComponentSharingUpdateSelf(ctx context.Context, req *model.ComponentSharingUpdateReq) (
+	resp *model.ComponentSharingUpdateResp, err error) {
+	req.Format()
+	if validateErr := req.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	globalQuery, globalQueryErr := utils.ConvertStructToQueryString(req.GlobalReq)
+	if globalQueryErr != nil {
+		err = globalQueryErr
+		return
+	}
+	req.GlobalReq.Clear()
+	headers := make(model.Headers)
+	headers.Json()
+	var result model.ComponentSharingUpdateResp
+	if requestErr := a.RequestPostJson(ctx, headers, model.ApiUrl3+"/component_sharing/update?"+globalQuery, req, &result); requestErr != nil {
+		err = requestErr
+		return
+	}
+	resp = &result
+	return
+}
+
+// ComponentSharingGetSelf 查询创意组件共享信息
+// https://developers.e.qq.com/v3.0/docs/api/component_sharing/get
+func (a *TencentAdapter) ComponentSharingGetSelf(ctx context.Context, req *model.ComponentSharingGetReq) (
+	resp *model.ComponentSharingGetResp, err error) {
+	req.Format()
+	if validateErr := req.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	var result model.ComponentSharingGetResp
+	if requestErr := a.RequestGet(ctx, nil, model.ApiUrl3+"/component_sharing/get", req, &result); requestErr != nil {
+		err = requestErr
+		return
+	}
+	resp = &result
+	return
+}
+
+// ComponentDetailGetSelf 获取创意组件详情
+// https://developers.e.qq.com/v3.0/docs/api/component_detail/get
+func (a *TencentAdapter) ComponentDetailGetSelf(ctx context.Context, req *model.ComponentDetailGetReq) (
+	resp *model.ComponentDetailGetResp, err error) {
+	req.Format()
+	if validateErr := req.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	var result model.ComponentDetailGetResp
+	if requestErr := a.RequestGet(ctx, nil, model.ApiUrl3+"/component_detail/get", req, &result); requestErr != nil {
+		err = requestErr
+		return
+	}
+	resp = &result
+	return
+}
