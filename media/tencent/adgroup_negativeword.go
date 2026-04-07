4e8bfd8e2388c2dec55c03dc8868e4c28c254357
tangping <442353346@qq.com>
2026-04-07 10:35:01 +0800

    A 腾讯广告新增广告否定词接口

---
+++ media/tencent/adgroup_negativeword.go
@@ -0,0 +1,34 @@
+package tencent
+
+import (
+	"context"
+
+	"github.com/cngamesdk/media-sdk/media/tencent/model"
+	"github.com/cngamesdk/media-sdk/utils"
+)
+
+// AdgroupNegativewordAddSelf 新增广告否定词
+// https://developers.e.qq.com/v3.0/docs/api/adgroup_negativewords/add
+func (a *TencentAdapter) AdgroupNegativewordAddSelf(ctx context.Context, req *model.AdgroupNegativewordAddReq) (
+	resp *model.AdgroupNegativewordAddResp, err error) {
+	req.Format()
+	if validateErr := req.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	globalQuery, globalQueryErr := utils.ConvertStructToQueryString(req.GlobalReq)
+	if globalQueryErr != nil {
+		err = globalQueryErr
+		return
+	}
+	req.GlobalReq.Clear()
+	headers := make(model.Headers)
+	headers.Json()
+	var result model.AdgroupNegativewordAddResp
+	if requestErr := a.RequestPostJson(ctx, headers, model.ApiUrl3+"/adgroup_negativewords/add?"+globalQuery, req, &result); requestErr != nil {
+		err = requestErr
+		return
+	}
+	resp = &result
+	return
+}
