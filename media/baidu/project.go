37937ccd950b864957639e6cda281986fe7a0c0a
tangping <442353346@qq.com>
2026-05-16 22:29:29 +0800

    A 百度营销-信息流广告投放-智投项目-更新项目

--- media/baidu/project.go
+++ media/baidu/project.go
@@ -40,3 +40,21 @@ func (a *BaiduAdapter) AddProjectFeedSelf(ctx context.Context, userName string, 
 	resp = &result
 	return
 }
+
+// UpdateProjectFeedSelf 更新项目
+// POST https://api.baidu.com/json/sms/service/ProjectFeedService/updateProjectFeed
+func (a *BaiduAdapter) UpdateProjectFeedSelf(ctx context.Context, userName string, accessToken string, req *model2.ProjectFeedUpdateReq) (resp *model2.ProjectFeedDataList, err error) {
+	req.Format()
+	if validateErr := req.Validate(); validateErr != nil {
+		err = validateErr
+		return
+	}
+	var result model2.ProjectFeedDataList
+	errRequest := a.RequestPostJsonBusiness(ctx, userName, accessToken, model2.BaseUrlAPI+model2.ProjectFeedUpdateServiceURL, req, &result)
+	if errRequest != nil {
+		err = errRequest
+		return
+	}
+	resp = &result
+	return
+}
